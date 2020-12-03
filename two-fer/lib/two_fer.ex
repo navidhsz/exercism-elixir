7678a747021de082eca2334de0659801cb7e6549
navidhsz <4723528+navidhsz@users.noreply.github.com>
2020-12-03 21:55:51 +0000

    add tow-fer

---
+++ two-fer/lib/two_fer.ex
@@ -0,0 +1,16 @@
+defmodule TwoFer do
+  @doc """
+  Two-fer or 2-fer is short for two for one. One for you and one for me.
+  """
+  @spec two_fer(String.t()) :: String.t()
+  def two_fer(name) do
+    cond do
+      name |> String.valid?() -> "One for " <> name <> ", one for me"
+      true -> raise FunctionClauseError
+    end
+  end
+
+  def two_fer() do
+    "One for you, one for me"
+  end
+end
