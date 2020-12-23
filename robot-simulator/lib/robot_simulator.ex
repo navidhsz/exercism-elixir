fb13d796bccf9605eb1d1111b0ec2bbdf18a5ef7
navidhsz <4723528+navidhsz@users.noreply.github.com>
2020-12-23 16:27:34 +0000

    refactor robot-simulator assignment

--- robot-simulator/lib/robot_simulator.ex
+++ robot-simulator/lib/robot_simulator.ex
@@ -1,25 +1,27 @@
 defmodule RobotSimulator do
   defstruct direction: nil, position: nil
 
+  defguard is_not_valid_direction(direction) when direction not in [:north, :south, :west, :east]
+
+  defguard is_not_valid_position(position)
+           when not (is_tuple(position) and tuple_size(position) == 2 and
+                       is_number(elem(position, 0)) and is_number(elem(position, 1)))
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ :north, position \\ {0, 0}) do
-    is_valid_direction = direction in [:north, :south, :west, :east]
 
-    is_valid_tuple =
-      is_tuple(position) and tuple_size(position) == 2 and is_number(elem(position, 0)) and
-        is_number(elem(position, 1))
+  def create(direction, position) when is_not_valid_direction(direction),
+    do: {:error, "invalid direction"}
 
-    cond do
-      is_valid_direction and not is_valid_tuple -> {:error, "invalid position"}
-      is_valid_tuple and not is_valid_direction -> {:error, "invalid direction"}
-      true -> %RobotSimulator{direction: direction, position: position}
-    end
-  end
+  def create(direction, position) when is_not_valid_position(position),
+    do: {:error, "invalid position"}
+
+  def create(direction \\ :north, position \\ {0, 0}),
+    do: %RobotSimulator{direction: direction, position: position}
 
   @doc """
   Simulate the robot's movement given a string of instructions.
@@ -30,48 +32,65 @@ defmodule RobotSimulator do
 
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
-    [h | t] = instructions |> String.codepoints()
-    new_instructions = t |> List.to_string()
+    [h | t] =
+      instructions
+      |> String.codepoints()
+
+    new_instructions =
+      t
+      |> List.to_string()
 
     {x, y} = robot.position
     direction = robot.direction
 
     case h do
       "R" when direction == :north ->
-        %{robot | direction: :east} |> simulate(new_instructions)
+        %{robot | direction: :east}
+        |> simulate(new_instructions)
 
       "R" when direction == :east ->
-        %{robot | direction: :south} |> simulate(new_instructions)
+        %{robot | direction: :south}
+        |> simulate(new_instructions)
 
       "R" when direction == :south ->
-        %{robot | direction: :west} |> simulate(new_instructions)
+        %{robot | direction: :west}
+        |> simulate(new_instructions)
 
       "R" when direction == :west ->
-        %{robot | direction: :north} |> simulate(new_instructions)
+        %{robot | direction: :north}
+        |> simulate(new_instructions)
 
       "L" when direction == :north ->
-        %{robot | direction: :west} |> simulate(new_instructions)
+        %{robot | direction: :west}
+        |> simulate(new_instructions)
 
       "L" when direction == :west ->
-        %{robot | direction: :south} |> simulate(new_instructions)
+        %{robot | direction: :south}
+        |> simulate(new_instructions)
 
       "L" when direction == :south ->
-        %{robot | direction: :east} |> simulate(new_instructions)
+        %{robot | direction: :east}
+        |> simulate(new_instructions)
 
       "L" when direction == :east ->
-        %{robot | direction: :north} |> simulate(new_instructions)
+        %{robot | direction: :north}
+        |> simulate(new_instructions)
 
       "A" when direction == :north ->
-        %{robot | position: {x, y + 1}} |> simulate(new_instructions)
+        %{robot | position: {x, y + 1}}
+        |> simulate(new_instructions)
 
       "A" when direction == :south ->
-        %{robot | position: {x, y - 1}} |> simulate(new_instructions)
+        %{robot | position: {x, y - 1}}
+        |> simulate(new_instructions)
 
       "A" when direction == :west ->
-        %{robot | position: {x - 1, y}} |> simulate(new_instructions)
+        %{robot | position: {x - 1, y}}
+        |> simulate(new_instructions)
 
       "A" when direction == :east ->
-        %{robot | position: {x + 1, y}} |> simulate(new_instructions)
+        %{robot | position: {x + 1, y}}
+        |> simulate(new_instructions)
 
       _ ->
         {:error, "invalid instruction"}
