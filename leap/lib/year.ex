688c14eda05695cdc8670ba0fc6e50316e834e35
navidhsz <4723528+navidhsz@users.noreply.github.com>
2020-12-27 18:44:40 +0000

    add leap assignment

---
+++ leap/lib/year.ex
@@ -0,0 +1,25 @@
+defmodule Year do
+  @doc """
+  Returns whether 'year' is a leap year.
+
+  A leap year occurs:
+
+  on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+      unless the year is also evenly divisible by 400
+  """
+  @spec leap_year?(non_neg_integer) :: boolean
+  def leap_year?(year) when rem(year, 100) == 0 do
+    case year do
+      year when rem(year, 400) == 0 -> true
+      _ -> false
+    end
+  end
+
+  def leap_year?(year) do
+    case year do
+      year when rem(year, 4) == 0 -> true
+      _ -> false
+    end
+  end
+end
